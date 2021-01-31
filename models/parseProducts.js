568a835102a66539cd4429164f00da0b29f7005a
Claudio Moreno <clauartur98@gmail.com>
2021-01-30 19:29:40 -0600

    feat: update final response

--- models/parseProducts.js
+++ models/parseProducts.js
@@ -1,15 +1,42 @@
 function parsefunction(objectFromRequest) {
-    let filters = objectFromRequest[0].available_filters;
-    let items = objectFromRequest[0].results.slice(0, 4);
 
-    return {
+    let categories = [];
+    let items = [];
+    let res = {}
+
+    let [response] = [...objectFromRequest]
+    response.available_filters[0].values.forEach(element => {
+        categories.push(element.name)
+    });
+
+    response.results.slice(0, 4).forEach(element => {
+        items.push({
+            'id': element.id,
+            'title': element.title,
+            'price': {
+                "currency": element.currency_id,
+                "amount": element.price,
+                "decimals": '00'
+            },
+            'picture': element.thumbnail,
+            "condition": element.condition,
+            "free_shipping": element.shipping.free_shipping
+
+        })
+    })
+
+    res = {
         'autor': {
-            "name": "Claudio",
-            "lastName": "Moreno"
+            'name': 'Claudio',
+            'lastName': 'Moreno'
         },
-        'categories': [...filters],
-        'items': [...items]
+        categories,
+        items
     }
+    return res;
 }
+
+
+
 
 module.exports = parsefunction;
